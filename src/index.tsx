a012aadbe0017f866e744b7bb1e94ae85b7b8929
Nik <nikfarce@gmail.com>
2022-05-27 22:44:34 +0800

    Created index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as ReactDOM from 'react-dom/client';
+
+ReactDOM
+  .createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(<h1>Hello World!</h1>);
